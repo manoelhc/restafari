ec3b0df9c31ac1d610f85dcf0283b8ec45fdb50c
Manoel Carvalho <manoelhc@gmail.com>
2016-06-26 15:24:39 -0300

    #3 Add new changes for Code Climate

--- ci/test.sh
+++ ci/test.sh
@@ -14,7 +14,7 @@ chmod +x ${_RESTAFARI_HOME}/build/restafari/restafari.py
 cd ${_RESTAFARI_HOME}
 
 for f in $(find test/rests/pass -name '*.rest'); do
-  $WARN "Running $f" ${_RESTAFARI_HOME}/build/restafari/restafari.py --port ${TEST_SERVER_PORT} --hostname localhost $f
+  $WARN "Running $f" coverage run ${_RESTAFARI_HOME}/build/restafari/restafari.py --port ${TEST_SERVER_PORT} --hostname localhost $f
 done
 
 kill $(ps aux | grep -F ${TEST_SERVER} | grep -v grep | awk '{print $2}') 2> /dev/null
