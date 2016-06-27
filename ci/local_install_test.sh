ccefa3d278ea0a689ddb0d9db70749dcb343365e
Manoel Carvalho <manoelhc@gmail.com>
2016-06-27 17:55:28 -0300

    #4 #3 Fix scripts to test PyPI package

--- ci/local_install_test.sh
+++ ci/local_install_test.sh
@@ -8,6 +8,8 @@ cd build
 
 python3 setup.py install
 
+echo "Python3 binary: $(which python3)"
+echo "PATH: ${PATH}"
 echo "Restafari binary: $(which restafari)"
 echo "PYTHONHOME=${PYTHONHOME}"
 echo "PYTHONPATH=${PYTHONPATH}"
