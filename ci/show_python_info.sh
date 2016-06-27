a33afa5fc3e1475cc7f69b07619e73fa62fa8377
Manoel Carvalho <manoelhc@gmail.com>
2016-06-27 18:07:24 -0300

    #3 Add this script to show some variable values.

---
+++ ci/show_python_info.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Python3 binary: $(which python3)"
+echo "PATH: ${PATH}"
+echo "Restafari binary: $(which restafari.py)"
+echo "PYTHONHOME=${PYTHONHOME}"
+echo "PYTHONPATH=${PYTHONPATH}"
