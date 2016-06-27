9899146a737b8bf7ea1c8e9ef617d62634190600
Manoel Carvalho <manoelhc@gmail.com>
2016-06-27 17:49:33 -0300

    #4 #3 Fix scripts to test PyPI package

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@
 
 cd ${_RESTAFARI_HOME}
 
-pip3 uninstall -y restafari
+# pip3 uninstall -y restafari
 
 [[ -d build/restafari ]] && rm -rf build/restafari
 
@@ -20,7 +20,6 @@ cp setup.cfg build/
 cp LICENSE build/LICENSE.txt
 cp README.md build/
 
-$SUCCESS "Checking Python 3 Dependencies" pip install -r ${_RESTAFARI_HOME}/requirements.pip
 $WARN "Checking code standards" /bin/bash ${_RESTAFARI_HOME}/test/standards.sh
 
 chmod +x build/restafari/restafari.py
--- ci/local_install_test.sh
+++ ci/local_install_test.sh
@@ -7,3 +7,7 @@ cd ${_RESTAFARI_HOME}
 cd build
 
 python3 setup.py install
+
+echo "Restafari binary: $(which restafari)"
+echo "PYTHONHOME=${PYTHONHOME}"
+echo "PYTHONPATH=${PYTHONPATH}"
