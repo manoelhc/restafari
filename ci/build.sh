940e09ef3c681635b9b7daa387a8d20783bf39c0
Manoel Carvalho <manoelhc@gmail.com>
2016-07-08 20:56:50 -0300

    #3 Add a new logic to set the release version automatically

--- ci/build.sh
+++ ci/build.sh
@@ -11,11 +11,30 @@ cd ${_RESTAFARI_HOME}
 mkdir -p build/restafari
 cp -r src/* build/restafari
 
-git tag | tail -n1 > build/VERSION
+cp_version=$(curl "https://pypi.python.org/pypi?name=restafari&:action=display_pkginfo" 2>/dev/null| egrep '^Version:' | awk '{print $2}')
+cg_version=$(git tag | tail -n1 | awk '{print $1}')
 
-cat build/VERSION
+cpv_major=$(echo ${cp_version} | cut -d. -f1)
+cpv_minor=$(echo ${cp_version} | cut -d. -f2)
+cpv_rel=$(echo ${cp_version} | cut -d. -f3)
 
-cp setup.py build/
+cpg_major=$(echo ${cg_version} | cut -d. -f1)
+cpg_minor=$(echo ${cg_version} | cut -d. -f2)
+cpg_rel=$(echo ${cg_version} | cut -d. -f3)
+
+build_version=
+
+if [[ ${cpv_major} -eq ${cpg_major} ]]; then
+  if [[ ${cpv_minor} -eq ${cpg_minor} ]]; then
+    build_version="${cpv_major}.${cpv_minor}.$(( ${cpv_rel} + 1 ))"
+  else
+    build_version="${cpv_major}.${cpg_minor}.$(( ${cpg_rel} + 1 ))"
+  fi
+else
+  build_version="${cpg_major}.${cpg_minor}.$(( ${cpg_rel} + 1 ))"
+fi
+
+cat setup.py | sed "s,{{RESTAFARI_VERSION}},${build_version},g" > build/setup.py
 cp setup.cfg build/
 cp LICENSE build/LICENSE.txt
 cp README.md build/
