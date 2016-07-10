8827694d394535e50e0766b5e8f5aa780837a21f
Manoel Carvalho <manoelhc@gmail.com>
2016-07-10 19:55:35 -0300

    Remove 'coverage run' until it work locally

--- ci/test.sh
+++ ci/test.sh
@@ -8,6 +8,7 @@ cd ${_RESTAFARI_HOME}
 # Test role processing
 
 for f in $(ls ${_RESTAFARI_HOME}/test/rests/pass/*.rest); do
-  $SUCCESS "Running $f" coverage run ${RESTAFARI_CMD} --port ${TEST_SERVER_PORT} --hostname localhost $f
+  $SUCCESS "Running $f" ${RESTAFARI_CMD} --port ${TEST_SERVER_PORT} --hostname localhost $f
+  [[ ${?} -gt 0 ]] && echo "Test failed!" && exit 1
 done
 echo "Test DONE"
