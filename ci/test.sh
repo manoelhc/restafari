2c3a4386c84c880f752276f2e05802c24842d0a4
Manoel Carvalho <manoelhc@gmail.com>
2016-07-10 20:04:55 -0300

    Add new TravisCi deploy parameters

--- ci/test.sh
+++ ci/test.sh
@@ -8,6 +8,7 @@ cd ${_RESTAFARI_HOME}
 # Test role processing
 
 for f in $(ls ${_RESTAFARI_HOME}/test/rests/pass/*.rest); do
+  # coverage3 run
   $SUCCESS "Running $f" ${RESTAFARI_CMD} --port ${TEST_SERVER_PORT} --hostname localhost $f
   [[ ${?} -gt 0 ]] && echo "Test failed!" && exit 1
 done
