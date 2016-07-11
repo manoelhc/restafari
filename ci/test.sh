a0dd59b6925e47f7e0a1eeb1f169d9a27ca2ed29
Manoel Carvalho <manoelhc@gmail.com>
2016-07-11 12:23:32 -0300

    Add the command output tests

--- ci/test.sh
+++ ci/test.sh
@@ -6,10 +6,16 @@
 cd ${_RESTAFARI_HOME}
 
 # Test role processing
-
 for f in $(ls ${_RESTAFARI_HOME}/test/rests/pass/*.rest); do
   # coverage3 run
   $SUCCESS "Running $f" ${RESTAFARI_CMD} --port ${TEST_SERVER_PORT} --hostname localhost $f
   [[ ${?} -gt 0 ]] && echo "Test failed!" && exit 1
 done
+
+# Test potential exeception failure
+for f in $(ls ${_RESTAFARI_HOME}/test/exceptions_test/*/run.sh); do
+  $SUCCESS "Running $f" bash ${f}
+  [[ ${?} -gt 0 ]] && echo "Test failed!" && exit 1
+done
+
 echo "Test DONE"
